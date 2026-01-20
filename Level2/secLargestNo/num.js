daa39ba69cac064b78602e9885babfb251a05b23
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-01-20 17:11:16 +0530

    Fix logic to correctly identify the second largest number in an array

--- Level2/secLargestNo/num.js
+++ Level2/secLargestNo/num.js
@@ -2,7 +2,7 @@ let arr = [10, 5, 20, 8, 3]
 let secLarg = 0
 for (let i = 0; i < arr.length; i++) {
   let largest = arr[0]
-  if (largest > arr[i+1]) {
+  if (arr[i+1] > largest) {
     secLarg = largest
     largest = arr[i+1]
   }else continue
