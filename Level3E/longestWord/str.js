42870ceb7581b69d56cf92c1e770986139c4506e
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-01-27 16:44:38 +0530

    Add implementation to find longest word in a sentence.

---
+++ Level3E/longestWord/str.js
@@ -0,0 +1,16 @@
+// Longest Word in a Sentence
+// "I love learning Javascript daily" → "Javascript"
+// You know this one; do it without looking at previous code.
+
+let sentence = "I love learning Javascript daily.";
+let spllitedStr = sentence.split(" ");
+let len = spllitedStr.length;
+let longestWord = spllitedStr[0];
+
+for (let i = 0; i < len; i++) {
+  let currentWord = spllitedStr[i];
+  if (currentWord.length > longestWord.length) {
+    longestWord = currentWord;
+  }
+}
+console.log(longestWord);
