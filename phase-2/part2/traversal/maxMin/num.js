385be2286c1f4383dd946246353a70ae5bada7d5
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-02-06 14:10:19 +0530

    Add implementation to find the maximum and minimum values in an array

---
+++ phase-2/part2/traversal/maxMin/num.js
@@ -0,0 +1,19 @@
+// Input: [3, 7, 1, 9, 4]
+// Output:
+// Max = 9
+// Min = 1
+
+let arr = [3, 7, 1, 9, 4];
+let max = arr[0];
+let min = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  } else if (arr[i] < min) {
+    min = arr[i];
+  }
+}
+
+console.log("max: ", max);
+console.log("min: ", min);
