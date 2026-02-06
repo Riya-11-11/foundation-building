d0679435cf4e22c918ca7f00071ce5a2ddf4f0ce
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-02-06 14:16:39 +0530

    Refactor conditional logic to improve readability in max/min calculation

--- phase-2/part2/traversal/maxMin/num.js
+++ phase-2/part2/traversal/maxMin/num.js
@@ -10,7 +10,8 @@ let min = arr[0];
 for (let i = 1; i < arr.length; i++) {
   if (arr[i] > max) {
     max = arr[i];
-  } else if (arr[i] < min) {
+  }
+  if (arr[i] < min) {
     min = arr[i];
   }
 }
