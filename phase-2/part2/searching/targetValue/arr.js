2db1df5ea44827c1d29dec5d6acc851021340277
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-02-07 22:04:25 +0530

    Add implementation to check for the existence of a target number in an array

---
+++ phase-2/part2/searching/targetValue/arr.js
@@ -0,0 +1,18 @@
+// let arr = [4, 7, 2, 9, 5]
+// and the target number 9,
+// check whether the number exists in the array or not.
+// Print "Found" or "Not Found".
+
+let arr = [4, 7, 2, 9, 5];
+let target = 9;
+let isFound = false;
+
+for (let i = 0; i < arr.length; i++) {
+  if (target === arr[i]) {
+    isFound = true;
+  }
+}
+
+if (isFound) {
+  console.log("Found");
+} else console.log("Not Found");
